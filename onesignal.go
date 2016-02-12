dc731d70d6ac368591a5680588ad54cf37ed7f52
Thomas Balthazar <thomas@balthazar.info>
2016-02-12 15:59:46 +0100

    Add NewRequest method

--- onesignal.go
+++ onesignal.go
@@ -1,12 +1,15 @@
 package onesignal
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/url"
 )
 
 const (
-	defaultBaseURL = "https://onesignal.com/api/v1"
+	defaultBaseURL = "https://onesignal.com/api/v1/"
 )
 
 type Client struct {
@@ -15,6 +18,7 @@ type Client struct {
 	Client  *http.Client
 }
 
+// NewClient returns a new OneSignal API client.
 func NewClient(key string, client *http.Client) *Client {
 	baseURL, _ := url.Parse(defaultBaseURL)
 	c := &Client{
@@ -24,4 +28,36 @@ func NewClient(key string, client *http.Client) *Client {
 	}
 
 	return c
+}
+
+// NewRequest creates an API request.
+func (c *Client) NewRequest(method, path string, body interface{}) (*http.Request, error) {
+	// build the URL
+	u, err := url.Parse(c.BaseURL.String() + path)
+	if err != nil {
+		return nil, err
+	}
+
+	// JSON encode the body
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// create the request
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	// headers
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", "Basic "+c.Key)
+
+	return req, nil
 }
--- onesignal_test.go
+++ onesignal_test.go
@@ -1,6 +1,7 @@
 package onesignal
 
 import (
+	"io/ioutil"
 	"net/http"
 	"testing"
 )
@@ -12,14 +13,59 @@ func TestNewClient(t *testing.T) {
 	c := NewClient(key, httpClient)
 
 	if got, want := c.BaseURL.String(), defaultBaseURL; got != want {
-		t.Errorf("NewClient BaseURL is %v, should be %v", got, want)
+		t.Errorf("NewClient BaseURL is %v, want %v", got, want)
 	}
 
 	if got, want := c.Key, key; got != want {
-		t.Errorf("NewClient Key is %v, should be %v", got, want)
+		t.Errorf("NewClient Key is %v, want %v", got, want)
 	}
 
 	if got, want := c.Client, httpClient; got != want {
-		t.Errorf("NewClient Client is %v, should be %v", got, want)
+		t.Errorf("NewClient Client is %v, want %v", got, want)
 	}
+}
+
+func TestNewRequest(t *testing.T) {
+	key := "fake key"
+	httpClient := &http.Client{}
+
+	c := NewClient(key, httpClient)
+
+	method := "GET"
+	inURL, outURL := "foo", defaultBaseURL+"foo"
+	inBody := struct{ Foo string }{Foo: "Bar"}
+	outBody := `{"Foo":"Bar"}` + "\n"
+	req, _ := c.NewRequest(method, inURL, inBody)
+
+	// test the HTTP method
+	if got, want := req.Method, method; got != want {
+		t.Errorf("NewRequest(%q) Method is %v, want %v", method, got, want)
+	}
+
+	// test the URL
+	if got, want := req.URL.String(), outURL; got != want {
+		t.Errorf("NewRequest(%q) URL is %v, want %v", inURL, got, want)
+	}
+
+	// test that body was JSON encoded
+	body, _ := ioutil.ReadAll(req.Body)
+	if got, want := string(body), outBody; got != want {
+		t.Errorf("NewRequest(%q) Body is %v, want %v", inBody, got, want)
+	}
+
+	// test Content-Type header
+	if got, want := req.Header.Get("Content-Type"), "application/json"; got != want {
+		t.Errorf("NewRequest() Content-Type header is %v, want %v", got, want)
+	}
+
+	// test Accept header
+	if got, want := req.Header.Get("Accept"), "application/json"; got != want {
+		t.Errorf("NewRequest() Accept header is %v, want %v", got, want)
+	}
+
+	// test Authorization header
+	if got, want := req.Header.Get("Authorization"), "Basic "+c.Key; got != want {
+		t.Errorf("NewRequest() Authorization header is %v, want %v", got, want)
+	}
+
 }
