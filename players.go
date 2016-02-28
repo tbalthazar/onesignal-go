d2d52cd61c67c1597f73396732654af8081d6cdd
Thomas Balthazar <thomas@balthazar.info>
2016-02-28 16:47:04 +0100

    Add a PlayerListOptions type
    Fixes #1

--- players.go
+++ players.go
@@ -3,3 +3,9 @@ package onesignal
 type PlayersService struct {
 	client *Client
 }
+
+type PlayerListOptions struct {
+	AppId  string `json:"app_id"`
+	limit  int    `json:"limit"`
+	offset int    `json:"offset"`
+}
