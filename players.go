1b0d622d1fbfb119d199322adab992d417d36019
Thomas Balthazar <thomas@balthazar.info>
2016-02-28 18:23:26 +0100

    Add a List method to the PlayersService [wip]
    Only send a basic GET request and doesn't parse the response.
    Basic tests with a http test server.
    
    Gitlab: #2

--- players.go
+++ players.go
@@ -1,11 +1,38 @@
 package onesignal
 
+import (
+	"log"
+)
+
 type PlayersService struct {
 	client *Client
 }
 
 type PlayerListOptions struct {
 	AppId  string `json:"app_id"`
-	limit  int    `json:"limit"`
-	offset int    `json:"offset"`
+	Limit  int    `json:"limit"`
+	Offset int    `json:"offset"`
+}
+
+func (s *PlayersService) List(opt *PlayerListOptions) {
+	req, err := s.client.NewRequest("GET", "/players", nil)
+	if err != nil {
+		log.Fatal("Do: ", err)
+	}
+
+	_, err = s.client.Client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+	}
+
+	// defer resp.Body.Close()
+
+	// var playerList PlayerList
+	// dec := json.NewDecoder(resp.Body)
+	// err = dec.Decode(&playerList)
+	// if err != nil {
+	// 	log.Fatal("JSON Decode error: ", err)
+	// }
+
+	// return &playerList
 }
--- players_test.go
+++ players_test.go
@@ -1,1 +1,43 @@
 package onesignal
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestList(t *testing.T) {
+	requestSent := false
+
+	// create a test server and a mux
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	// create a client, giving it the test server URL
+	key := "fake-key"
+	client := NewClient(key, http.DefaultClient)
+	url, _ := url.Parse(server.URL)
+	client.BaseURL = url
+
+	mux.HandleFunc("/players", func(w http.ResponseWriter, r *http.Request) {
+		requestSent = true
+		want := "GET"
+		if got := r.Method; got != want {
+			t.Errorf("Request method: %v, want %v", got, want)
+		}
+		// fmt.Fprint(w, `[{"number":1}]`)
+	})
+
+	opt := &PlayerListOptions{
+		AppId:  "fake-app-id",
+		Limit:  10,
+		Offset: 0,
+	}
+	client.Players.List(opt)
+
+	if requestSent == false {
+		t.Errorf("Request has not been sent")
+	}
+}
