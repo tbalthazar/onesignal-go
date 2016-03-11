7be75beac7bd4d0989039e267a90a5fcad819a02
Thomas Balthazar <thomas@balthazar.info>
2016-03-11 14:24:27 +0100

    Use godotenv to load api key in the example

--- example/example.go
+++ example/example.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"fmt"
-	"github.com/tbalthazar/onesignal-go"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/tbalthazar/onesignal-go"
 )
 
 func main() {
-	fmt.Println("--- hey")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	appID := os.Getenv("ONESIGNAL_APP_ID")
+	appKey := os.Getenv("ONESIGNAL_API_KEY")
+
 	opt := &onesignal.PlayerListOptions{
-		AppId:  "fake-app-id",
+		AppId:  appID,
 		Limit:  10,
 		Offset: 0,
 	}
 	client := onesignal.NewClient(nil)
+	client.AppKey = appKey
+
 	listRes, res, err := client.Players.List(opt)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("--- listRes:%+v", listRes)
+	fmt.Printf("--- listRes:%+v\n", listRes)
 	fmt.Printf("--- res:%+v, err:%+v\n", res)
-	fmt.Printf("--- err:%+v\n", err)
 }
