4fc8f969eabaedfef442b7805eddfe42ce7a95d6
Thomas Balthazar <thomas@balthazar.info>
2016-03-16 12:43:14 +0100

    Extract some struct/string creation

--- players_test.go
+++ players_test.go
@@ -45,6 +45,87 @@ func testBody(t *testing.T, r *http.Request, body interface{}, want interface{})
 	}
 }
 
+func samplePlayerRequest() *PlayerRequest {
+	return &PlayerRequest{
+		Identifier:   "fake-identifier",
+		Language:     "fake-language",
+		Timezone:     -28800,
+		GameVersion:  "1.0",
+		DeviceOS:     "iOS",
+		DeviceModel:  "iPhone5,2",
+		AdID:         "fake-ad-id",
+		SDK:          "fake-sdk",
+		SessionCount: 1,
+		Tags: map[string]string{
+			"a":   "1",
+			"foo": "bar",
+		},
+		AmountSpent:       0,
+		CreatedAt:         1395096859,
+		Playtime:          12,
+		BadgeCount:        1,
+		LastActive:        1395096859,
+		TestType:          1,
+		NotificationTypes: "2",
+	}
+}
+
+func samplePlayerListResponse() string {
+	return `{
+		  "total_count":2,
+		  "offset":0,
+		  "limit":10,
+		  "players":
+		  [
+		     {
+					 "id": "id123",
+					 "playtime": 0,
+					 "sdk": "fake-sdk",
+		       "identifier":"ce777617da7f548fe7a9ab6febb56cf39fba6d382000c0395666288d961ee566",
+		       "session_count":1,
+		       "language":"en",
+		       "timezone":-28800,
+		       "game_version":"1.0",
+		       "device_os":"7.0.4",
+		       "device_type":0,
+		       "device_model":"iPhone",
+		       "ad_id":null,
+		       "tags":{"a":"1","foo":"bar"},
+		       "last_active":1395096859,
+		       "amount_spent":0.0,
+		       "created_at":1395096859,
+		       "invalid_identifier":false,
+		       "badge_count": 0
+		     }
+		  ]
+	}`
+}
+
+func samplePlayer() *Player {
+	return &Player{
+		ID:           "id123",
+		Playtime:     0,
+		SDK:          "fake-sdk",
+		Identifier:   "ce777617da7f548fe7a9ab6febb56cf39fba6d382000c0395666288d961ee566",
+		SessionCount: 1,
+		Language:     "en",
+		Timezone:     -28800,
+		GameVersion:  "1.0",
+		DeviceOS:     "7.0.4",
+		DeviceType:   0,
+		DeviceModel:  "iPhone",
+		Tags: map[string]string{
+			"a":   "1",
+			"foo": "bar",
+		},
+		LastActive:        1395096859,
+		AmountSpent:       0.0,
+		CreatedAt:         1395096859,
+		InvalidIdentifier: false,
+		BadgeCount:        0,
+	}
+}
+
 func TestList(t *testing.T) {
 	setup()
 	defer teardown()
@@ -77,34 +158,7 @@ func TestList(t *testing.T) {
 			t.Errorf("URL: got %v, want %v", got, want)
 		}
 
-		fmt.Fprint(w, `{
-		  "total_count":2,
-		  "offset":0,
-		  "limit":10,
-		  "players":
-		  [
-		     {
-					 "id": "id123",
-					 "playtime": 0,
-					 "sdk": "fake-sdk",
-		       "identifier":"ce777617da7f548fe7a9ab6febb56cf39fba6d382000c0395666288d961ee566",
-		       "session_count":1,
-		       "language":"en",
-		       "timezone":-28800,
-		       "game_version":"1.0",
-		       "device_os":"7.0.4",
-		       "device_type":0,
-		       "device_model":"iPhone",
-		       "ad_id":null,
-		       "tags":{"a":"1","foo":"bar"},
-		       "last_active":1395096859,
-		       "amount_spent":0.0,
-		       "created_at":1395096859,
-		       "invalid_identifier":false,
-		       "badge_count": 0
-		     }
-		  ]
-			}`)
+		fmt.Fprint(w, samplePlayerListResponse())
 	})
 
 	listRes, _, err := client.Players.List(opt)
@@ -112,28 +166,7 @@ func TestList(t *testing.T) {
 		t.Errorf("List returned an error: %v", err)
 	}
 
-	player := Player{
-		ID:           "id123",
-		Playtime:     0,
-		SDK:          "fake-sdk",
-		Identifier:   "ce777617da7f548fe7a9ab6febb56cf39fba6d382000c0395666288d961ee566",
-		SessionCount: 1,
-		Language:     "en",
-		Timezone:     -28800,
-		GameVersion:  "1.0",
-		DeviceOS:     "7.0.4",
-		DeviceType:   0,
-		DeviceModel:  "iPhone",
-		Tags: map[string]string{
-			"a":   "1",
-			"foo": "bar",
-		},
-		LastActive:        1395096859,
-		AmountSpent:       0.0,
-		CreatedAt:         1395096859,
-		InvalidIdentifier: false,
-		BadgeCount:        0,
-	}
+	player := *samplePlayer()
 	want := &PlayerListResponse{
 		TotalCount: 2,
 		Offset:     0,
@@ -192,37 +225,14 @@ func TestCreate(t *testing.T) {
 	setup()
 	defer teardown()
 
-	// PlayerRequest
-	player := &PlayerRequest{
-		AppID:        "fake-app-id",
-		DeviceType:   0,
-		Identifier:   "fake-identifier",
-		Language:     "fake-language",
-		Timezone:     -28800,
-		GameVersion:  "1.0",
-		DeviceOS:     "iOS",
-		DeviceModel:  "iPhone5,2",
-		AdID:         "fake-ad-id",
-		SDK:          "fake-sdk",
-		SessionCount: 1,
-		Tags: map[string]string{
-			"a":   "1",
-			"foo": "bar",
-		},
-		AmountSpent: 0,
-		CreatedAt:   1395096859,
-		Playtime:    12,
-		BadgeCount:  1,
-		LastActive:  1395096859,
-		TestType:    1,
-	}
+	playerRequest := samplePlayerRequest()
 
 	mux.HandleFunc("/players", func(w http.ResponseWriter, r *http.Request) {
 		requestSent = true
 
 		testMethod(t, r, "POST")
 
-		testBody(t, r, &PlayerRequest{}, player)
+		testBody(t, r, &PlayerRequest{}, playerRequest)
 
 		fmt.Fprint(w, `{
 			"success": true,
@@ -230,7 +240,7 @@ func TestCreate(t *testing.T) {
 		}`)
 	})
 
-	createRes, _, _ := client.Players.Create(player)
+	createRes, _, _ := client.Players.Create(playerRequest)
 	want := &PlayerCreateResponse{
 		Success: true,
 		ID:      "ffffb794-ba37-11e3-8077-031d62f86ebf",
@@ -250,43 +260,21 @@ func TestUpdate(t *testing.T) {
 	setup()
 	defer teardown()
 
-	// PlayerRequest
-	player := &PlayerRequest{
-		Identifier:   "fake-identifier",
-		Language:     "fake-language",
-		Timezone:     -28800,
-		GameVersion:  "1.0",
-		DeviceOS:     "iOS",
-		DeviceModel:  "iPhone5,2",
-		AdID:         "fake-ad-id",
-		SDK:          "fake-sdk",
-		SessionCount: 1,
-		Tags: map[string]string{
-			"a":   "1",
-			"foo": "bar",
-		},
-		AmountSpent:       0,
-		CreatedAt:         1395096859,
-		Playtime:          12,
-		BadgeCount:        1,
-		LastActive:        1395096859,
-		TestType:          1,
-		NotificationTypes: "2",
-	}
+	playerRequest := samplePlayerRequest()
 
 	mux.HandleFunc("/players/fake-id", func(w http.ResponseWriter, r *http.Request) {
 		requestSent = true
 
 		testMethod(t, r, "PUT")
 
-		testBody(t, r, &PlayerRequest{}, player)
+		testBody(t, r, &PlayerRequest{}, playerRequest)
 
 		fmt.Fprint(w, `{
 			"success": true
 		}`)
 	})
 
-	updateRes, _, _ := client.Players.Update("fake-id", player)
+	updateRes, _, _ := client.Players.Update("fake-id", playerRequest)
 	want := &PlayerUpdateResponse{
 		Success: true,
 	}
